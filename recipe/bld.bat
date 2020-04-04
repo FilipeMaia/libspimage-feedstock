c7d6ecd2bd0c6598c2c29c76e4c9bed2edb8d0b2
Filipe Maia <filipe.c.maia@gmail.com>
2020-04-04 18:52:31 +0200

    Try to fix the windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,8 @@
 setlocal EnableDelayedExpansion
+:: Remove -GL from CXXFLAGS as this causes a fatal error when combined with WINDOWS_EXPORT_ALL_SYMBOLS
+set "CXXFLAGS=%CXXFLAGS:-GL=%"
+:: Remove -GL from CFLAGS as this causes a fatal error when combined with WINDOWS_EXPORT_ALL_SYMBOLS
+set "CFLAGS=%CFLAGS:-GL=%"
 
 :: Make a build folder and change to it.
 mkdir build
